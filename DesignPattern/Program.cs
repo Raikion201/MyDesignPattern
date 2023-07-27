796e860ac41005094c831e962eefe86e241b2176
raikion201 <haitrieukt123@gmail.com>
2023-07-27 08:46:17 +0700

    init-singleton

---
+++ DesignPattern/Program.cs
@@ -0,0 +1,14 @@
+﻿
+
+using DesignPattern;
+using DesignPattern.Singleton;
+
+Singleton object1 = Singleton.Instance();
+Singleton object2 = Singleton.Instance();
+
+if (object1 == object2)
+{
+    Console.WriteLine("Test");
+}
+
+int x = 0;
---
+++ DesignPattern/Singleton/Singleton.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Singleton
+{
+    public class Singleton
+    {
+        static Singleton instance;
+        public string Setting { get; set; } = "Color Blue";
+
+        public Singleton()
+        {
+            
+        }
+
+        public static Singleton Instance()
+        {
+            if (instance == null)
+            {
+                instance = new Singleton();
+            }
+            return instance;
+        }
+    }
+}
