687cede30102d1f0f92a632559b6ea8b83722e83
raikion201 <haitrieukt123@gmail.com>
2023-08-05 15:42:01 +0700

    composite

---
+++ DesignPattern/Structural/Bridge.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPattern.Structural
+{
+    class Abstraction
+    {
+        protected IImplementation _implementation;
+
+        public Abstraction(IImplementation implementation)
+        {
+            this._implementation = implementation;
+        }
+
+        public virtual string Operation()
+        {
+            return "Abstract: Base operation with:\n" +
+                _implementation.OperationImplementation();
+        }
+    }
+
+    public interface IImplementation
+    {
+        string OperationImplementation();
+    }
+
+    class ExtendedAbstraction : Abstraction
+    {
+        public ExtendedAbstraction(IImplementation implementation) : base(implementation)
+        {
+        }
+
+        public override string Operation()
+        {
+            return "ExtendedAbstraction: Extended operation with:\n" +
+                base._implementation.OperationImplementation();
+        }
+    }
+
+    class ConcreteImplementationA : IImplementation
+    {
+        public string OperationImplementation()
+        {
+            return "ConcreteImplementationA: The result in platform A.\n";
+        }
+    }
+
+    class Client
+    {
+        // Except for the initialization phase, where an Abstraction object gets
+        // linked with a specific Implementation object, the client code should
+        // only depend on the Abstraction class. This way the client code can
+        // support any abstraction-implementation combination.
+        public void ClientCode(Abstraction abstraction)
+        {
+            Console.Write(abstraction.Operation());
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Client client = new Client();
+
+            Abstraction abstraction;
+            // The client code should be able to work with any pre-configured
+            // abstraction-implementation combination.
+            abstraction = new Abstraction(new ConcreteImplementationA());
+            client.ClientCode(abstraction);
+
+            Console.WriteLine();
+
+            abstraction = new ExtendedAbstraction(new ConcreteImplementationB());
+            client.ClientCode(abstraction);
+        }
+    }
+}
